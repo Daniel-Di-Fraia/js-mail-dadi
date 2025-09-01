071cd4fbcf7cb84d1d6e377d8b0f027f6bdc5f4c
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2025-09-01 15:09:27 +0200

    finalizzazione esercizio dadi random

--- js/dadi.js
+++ js/dadi.js
@@ -5,3 +5,24 @@
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Che ci sia un array da qualche parte?
 // Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+
+//facciamo generare al programma 2 numeri uno per l'utente
+//e uno per la macchina da 1 a 6, come se fosse un dado
+const pcNumber = Math.floor(Math.random() * 6) + 1;
+const userNumber = Math.floor(Math.random() * 6) + 1;
+
+console.log("Hai tirato il dado e hai fatto:" , userNumber);
+console.log("La macchina ha tirato il dado e ha fatto:" , pcNumber);
+
+//stabiliamo chi ha vinto tramite il numero random
+//potreste anche pareggiare
+if (userNumber > pcNumber){
+    console.log("Complimenti! hai vinto superando il dado della macchina!");
+} else if (userNumber === pcNumber){
+    console.log("Tu e la macchina avete pareggiato!");
+} else {
+    console.log("Ah! La macchina ti ha battuto!");
+}
+
+
