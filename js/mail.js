56e850a992a8c22f833896165893781eb62e6c4f
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2025-09-01 14:52:28 +0200

    finalizzazione esercizio Mail festa

--- js/mail.js
+++ js/mail.js
@@ -9,3 +9,37 @@
 // Non è necessario provvedere alla validazione delle email
 
 
+//creazione array mail registrate sul portale
+const mail = [
+    "franscescofava@gmail.com",
+    "lucaesposito@libero.it",
+    "noemilucciola@gmail.com",
+    "alessandroverdi@yahoo.it",
+]
+//booleano per capire se ha trovato o meno la mail
+let find = false;
+
+//facciamo inserire all'utente la propria mail per verificare se presente in lista
+const userMail = prompt("Inserisci la tua E-mail");
+
+//controllo elementi lista tramite ciclo for
+for (let i = 0; i < mail.length; i++) {
+    if (mail[i] === userMail) {
+        find = true;
+        }
+}
+
+//verifica se la mail è presente sulla lista
+if (find === true) {
+    console.log("La mail" , userMail , "è presente sulla lista, può entrare!");
+} else {
+     console.log("La mail" , userMail , "non è presente sulla lista, non può entrare.");
+}
+
+
+
+
+
+
+
+
